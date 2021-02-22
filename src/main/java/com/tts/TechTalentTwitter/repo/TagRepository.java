41e79ab9dbb651dacfc12e987d50adec8f62dd78
shantel-dot <taylorteep2@yahoo.com>
2021-02-22 14:37:01 -0600

    Initial commit-updated twitter project second commit-new updates

---
+++ src/main/java/com/tts/TechTalentTwitter/repo/TagRepository.java
@@ -0,0 +1,11 @@
+package com.tts.TechTalentTwitter.repo;
+
+
+import com.tts.TechTalentTwitter.model.Tag;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepository extends CrudRepository<Tag, Long> {
+    Tag findByPhrase(String phrase);
+}
